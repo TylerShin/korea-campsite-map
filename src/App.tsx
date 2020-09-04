d02b5b57d33e2aabd49536183abb85f460c65f5e
Tyler Shin <shincode@icloud.com>
2020-09-04 18:41:27 +0900

    add infowindow

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,33 @@ function App() {
     const map = new kakao.maps.Map(container, options);
 
     campingSiteData.map((camp) => {
-      const markerPosition = new kakao.maps.LatLng(camp["위도"], camp["경도"]);
-      const marker = new kakao.maps.Marker({
-        position: markerPosition,
-      });
+      const position = new kakao.maps.LatLng(camp["위도"], camp["경도"]);
+      const marker = new kakao.maps.Marker({ position });
 
       marker.setMap(map);
+      // 인포윈도우를 생성합니다
+      var iwContent = `
+        <div style="padding:4px;">
+          <div><b>${camp["캠핑(야영)장명"]}</b></div>
+          <div>${camp["캠핑(야영)장구분"]}</div>
+        </div>
+      `; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+      var infowindow = new kakao.maps.InfoWindow({
+        content: iwContent,
+      });
+
+      // 마커에 마우스오버 이벤트를 등록합니다
+      kakao.maps.event.addListener(marker, "mouseover", function () {
+        // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+        infowindow.open(map, marker);
+      });
+
+      // 마커에 마우스아웃 이벤트를 등록합니다
+      kakao.maps.event.addListener(marker, "mouseout", function () {
+        // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+        infowindow.close();
+      });
+
       return marker;
     });
   }, []);
