459b1457bd4a2f1d463d9d5248c71eccf388b8dd
Tyler Shin <shincode@icloud.com>
2020-09-05 00:10:57 +0900

    add type filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React, { useEffect } from "react";
-import { ThemeProvider, CSSReset, Heading, Box } from "@chakra-ui/core";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  ThemeProvider,
+  CSSReset,
+  Heading,
+  Box,
+  Stack,
+  Checkbox,
+} from "@chakra-ui/core";
 import campingSiteData from "./data/camping.json";
 
-declare var kakao: any;
+declare const kakao: any;
+
+type CAMPSITE_TYPE = "일반야영장" | "자동차야영장" | "글램핑" | "카라반";
 
 function App() {
+  const [campsiteList, setCampsiteList] = useState(campingSiteData);
+  const [selectedCampsiteType, setSelectedCampsiteType] = useState<
+    CAMPSITE_TYPE[]
+  >(["일반야영장", "자동차야영장", "글램핑", "카라반"]);
+  const map = useRef<any>();
+  const markers = useRef<{ marker: any; camp: any; infoWindow: any }[]>();
+
   useEffect(() => {
     const container = document.getElementById("camp-map");
     const options = {
@@ -12,44 +28,96 @@ function App() {
       level: 3,
     };
 
-    const map = new kakao.maps.Map(container, options);
+    map.current = new kakao.maps.Map(container, options);
+  }, [campsiteList]);
 
-    campingSiteData.map((camp) => {
-      const position = new kakao.maps.LatLng(camp["위도"], camp["경도"]);
-      const marker = new kakao.maps.Marker({ position });
+  useEffect(() => {
+    const markerList = !markers.current
+      ? campsiteList.map((camp) => {
+          const position = new kakao.maps.LatLng(camp["위도"], camp["경도"]);
+          const marker = new kakao.maps.Marker({ position });
+          const iwContent = `
+          <div style="padding:4px;">
+            <div><b>${camp["캠핑(야영)장명"]}</b></div>
+            <div>${camp["캠핑(야영)장구분"]}</div>
+          </div>
+        `;
+          const infoWindow = new kakao.maps.InfoWindow({ content: iwContent });
 
-      marker.setMap(map);
-      // 인포윈도우를 생성합니다
-      var iwContent = `
-        <div style="padding:4px;">
-          <div><b>${camp["캠핑(야영)장명"]}</b></div>
-          <div>${camp["캠핑(야영)장구분"]}</div>
-        </div>
-      `; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-      var infowindow = new kakao.maps.InfoWindow({
-        content: iwContent,
-      });
+          kakao.maps.event.addListener(marker, "mouseover", () => {
+            console.log("fire mouseover");
+            infoWindow.open(map.current, marker);
+          });
 
-      // 마커에 마우스오버 이벤트를 등록합니다
-      kakao.maps.event.addListener(marker, "mouseover", function () {
-        // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-        infowindow.open(map, marker);
-      });
+          kakao.maps.event.addListener(marker, "mouseout", () => {
+            infoWindow.close();
+          });
 
-      // 마커에 마우스아웃 이벤트를 등록합니다
-      kakao.maps.event.addListener(marker, "mouseout", function () {
-        // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
-        infowindow.close();
-      });
+          return { marker, camp, infoWindow };
+        })
+      : markers.current;
 
-      return marker;
+    markerList.forEach(({ marker, camp }) => {
+      if (
+        selectedCampsiteType.includes(camp["캠핑(야영)장구분"] as CAMPSITE_TYPE)
+      ) {
+        marker.setMap(map.current);
+      } else {
+        marker.setMap(null);
+      }
     });
-  }, []);
+
+    markers.current = markerList;
+  }, [selectedCampsiteType, campsiteList]);
+
+  function handleClickCheckbox(type: CAMPSITE_TYPE) {
+    if (selectedCampsiteType.includes(type)) {
+      const i = selectedCampsiteType.indexOf(type);
+      setSelectedCampsiteType([
+        ...selectedCampsiteType.slice(0, i),
+        ...selectedCampsiteType.slice(i + 1),
+      ]);
+    } else {
+      setSelectedCampsiteType([type, ...selectedCampsiteType]);
+    }
+  }
 
   return (
     <ThemeProvider>
       <CSSReset />
       <Heading as="h1">전국 캠프사이트 현황</Heading>
+      <Box my={4}>
+        <Stack spacing={10} isInline>
+          <Checkbox
+            size="lg"
+            onChange={() => handleClickCheckbox("일반야영장")}
+            isChecked={selectedCampsiteType.includes("일반야영장")}
+          >
+            일반야영장
+          </Checkbox>
+          <Checkbox
+            size="lg"
+            onChange={() => handleClickCheckbox("자동차야영장")}
+            isChecked={selectedCampsiteType.includes("자동차야영장")}
+          >
+            자동차야영장
+          </Checkbox>
+          <Checkbox
+            size="lg"
+            onChange={() => handleClickCheckbox("글램핑")}
+            isChecked={selectedCampsiteType.includes("글램핑")}
+          >
+            글램핑
+          </Checkbox>
+          <Checkbox
+            size="lg"
+            onChange={() => handleClickCheckbox("카라반")}
+            isChecked={selectedCampsiteType.includes("카라반")}
+          >
+            카라반
+          </Checkbox>
+        </Stack>
+      </Box>
       <Box>
         <Box id="camp-map" w="100%" h="80vh" />
       </Box>
