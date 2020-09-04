9baf6233547da6f8293b80ac08405ed591564718
Tyler Shin <shincode@icloud.com>
2020-09-04 16:10:57 +0900

    init repo & add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { ThemeProvider, CSSReset, Heading, Box } from "@chakra-ui/core";
+import campingSiteData from "./data/camping.json";
+
+declare var kakao: any;
+
+console.log(campingSiteData);
 
 function App() {
+  useEffect(() => {
+    const container = document.getElementById("camp-map");
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+
+    const map = new kakao.maps.Map(container, options);
+    console.log(map);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <CSSReset />
+      <Heading as="h1">전국 캠프사이트 현황</Heading>
+      <Box>
+        <Box id="camp-map" w="640px" h="480px" />
+      </Box>
+    </ThemeProvider>
   );
 }
 
