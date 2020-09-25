646ff850dcf4abea84325f44353cd043e3f238c4
Tyler Shin <shincode@icloud.com>
2020-09-25 09:01:06 +0900

    Add basic filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ThemeProvider, CSSReset, Heading, Box } from "@chakra-ui/core";
 import campingSiteData from "./data/camping.json";
 
 declare var kakao: any;
 
+type CampType = "일반야영장" | "자동차야영장" | "글램핑" | "카라반";
+
 function App() {
+  const [typeFilter, setTypeFilter] = useState<CampType[]>([
+    "일반야영장",
+    "자동차야영장",
+    "글램핑",
+    "카라반",
+  ]);
+  const kakaoMap = useRef<any>(null);
+
   useEffect(() => {
     const container = document.getElementById("camp-map");
     const options = {
@@ -12,44 +22,59 @@ function App() {
       level: 3,
     };
 
-    const map = new kakao.maps.Map(container, options);
+    kakaoMap.current = new kakao.maps.Map(container, options);
+  }, []);
 
-    campingSiteData.map((camp) => {
-      const position = new kakao.maps.LatLng(camp["위도"], camp["경도"]);
-      const marker = new kakao.maps.Marker({ position });
+  useEffect(() => {
+    const markers = campingSiteData
+      .filter((camp) =>
+        typeFilter.some((campType) =>
+          camp["캠핑(야영)장구분"].includes(campType)
+        )
+      )
+      .map((camp) => {
+        const position = new kakao.maps.LatLng(camp["위도"], camp["경도"]);
+        const marker = new kakao.maps.Marker({ position });
 
-      marker.setMap(map);
-      // 인포윈도우를 생성합니다
-      var iwContent = `
+        marker.setMap(kakaoMap.current);
+
+        const iwContent = `
         <div style="padding:4px;">
           <div><b>${camp["캠핑(야영)장명"]}</b></div>
           <div>${camp["캠핑(야영)장구분"]}</div>
         </div>
-      `; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-      var infowindow = new kakao.maps.InfoWindow({
-        content: iwContent,
+      `;
+        const infowindow = new kakao.maps.InfoWindow({
+          content: iwContent,
+        });
+
+        kakao.maps.event.addListener(marker, "mouseover", function () {
+          infowindow.open(kakaoMap.current, marker);
+        });
+
+        kakao.maps.event.addListener(marker, "mouseout", function () {
+          infowindow.close();
+        });
+
+        return marker;
       });
 
-      // 마커에 마우스오버 이벤트를 등록합니다
-      kakao.maps.event.addListener(marker, "mouseover", function () {
-        // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-        infowindow.open(map, marker);
-      });
-
-      // 마커에 마우스아웃 이벤트를 등록합니다
-      kakao.maps.event.addListener(marker, "mouseout", function () {
-        // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
-        infowindow.close();
-      });
-
-      return marker;
-    });
-  }, []);
+    return () => {
+      markers.forEach((marker) => marker.setMap(null));
+    };
+  }, [typeFilter]);
 
   return (
     <ThemeProvider>
       <CSSReset />
-      <Heading as="h1">전국 캠프사이트 현황</Heading>
+      <button
+        onClick={() => {
+          setTypeFilter(["일반야영장"]);
+        }}
+      >
+        일반야영장만 보기
+      </button>
+      <Heading as="h1">2020 전국 캠프사이트</Heading>
       <Box>
         <Box id="camp-map" w="100%" h="80vh" />
       </Box>
